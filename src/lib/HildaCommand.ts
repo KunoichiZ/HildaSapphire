4ffaa91ee2cf9a248a262c310264d37a3cd08af7
KunoichiZ <KunoichiZ@users.noreply.github.com>
2021-10-13 22:29:54 -0500

    remove fullCategory stuff

--- src/lib/HildaCommand.ts
+++ src/lib/HildaCommand.ts
@@ -1,29 +1,24 @@
 // Command file from godfather (https://github.com/Stitch07/godfather) Copyright 2020 Stitch07, used under the AGPL-3.0 License
 import { Command, CommandOptions, PieceContext } from '@sapphire/framework';
-import { sep } from 'path';
+// import { sep } from 'path';
 
-export default abstract class SapphireCommand extends Command {
-	declare public fullCategory: string[];
+export default abstract class HildaCommand extends Command {
+	// declare public fullCategory: string[];
 
 	public constructor(context: PieceContext, { name, ...options }: CommandOptions) {
 		super(context, { name, ...options });
-
-		const paths = context.path.split(sep);
-
-		this.fullCategory = paths.slice(paths.indexOf('commands') + 1, -1);
 	}
 
-	public get category() {
-		return this.fullCategory[0] ?? 'General';
-	}
+	// public get category() {
+	// 	return this.fullCategory[0] ?? 'General';
+	// }
 
-	/**
-	 * The sub category for the command
-	 * @since 0.0.1
-	 * @type {string}
-	 * @readonly
-	 */
-	public get subCategory() {
-		return this.fullCategory[1] ?? 'General';
-	}
+	// /**
+	//  * The sub category for the command
+	//  * @since 0.0.1
+	//  * @type {string}
+	//  */
+	// public get subCategory() {
+	// 	return this.fullCategory[1] ?? 'General';
+	// }
 }
