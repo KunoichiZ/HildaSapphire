eb7beebb3578f5ce0c36e1d788dd26b44b6dc431
KunoichiZ <KunoichiZ@users.noreply.github.com>
2021-10-26 16:05:51 -0500

    Update HildaCommand.ts

--- src/lib/HildaCommand.ts
+++ src/lib/HildaCommand.ts
@@ -1,24 +1,8 @@
-// Command file from godfather (https://github.com/Stitch07/godfather) Copyright 2020 Stitch07, used under the AGPL-3.0 License
 import { Command, CommandOptions, PieceContext } from '@sapphire/framework';
-// import { sep } from 'path';
 
 export default abstract class HildaCommand extends Command {
-	// declare public fullCategory: string[];
 
 	public constructor(context: PieceContext, { name, ...options }: CommandOptions) {
 		super(context, { name, ...options });
 	}
-
-	// public get category() {
-	// 	return this.fullCategory[0] ?? 'General';
-	// }
-
-	// /**
-	//  * The sub category for the command
-	//  * @since 0.0.1
-	//  * @type {string}
-	//  */
-	// public get subCategory() {
-	// 	return this.fullCategory[1] ?? 'General';
-	// }
 }
